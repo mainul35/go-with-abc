124b231b99b2a39ae8f82d150f688c085fcc9eb4
mainul35 <mainuls18@gmail.com>
2023-03-21 23:50:13 +0700

    updated exercise

--- main/back-to-basics/conditions/switch-case/ScoreCalculator.go
+++ main/back-to-basics/conditions/switch-case/ScoreCalculator.go
@@ -1,16 +1,25 @@
 package main
+
 import "fmt"
 
 func main() {
 
-    switch result := calculateResult(10); {
-        case (result < 39) : 
-            fmt.Println("Failed")
-        default: 
-            fmt.Println("Passed")
-    }
+	switch result := calculateResult(10); {
+	case (result < 39):
+		fmt.Println("Failed")
+	case result >= 40 && result <= 49:
+		fmt.Println("Passed with a D")
+	case result >= 50 && result <= 59:
+		fmt.Println("Passed with a C")
+	case result >= 60 && result <= 69:
+		fmt.Println("Passed with a B")
+	case result >= 70 && result <= 100:
+		fmt.Println("Passed with a A")
+	default:
+		fmt.Println("Passed")
+	}
 }
 
 func calculateResult(score int) int {
-    return 20 + score
+	return 20 + score
 }
