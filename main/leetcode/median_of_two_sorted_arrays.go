633348188d93db1ee0f39cfd5abf2324d8168d01
mainul35 <mainuls18@gmail.com>
2024-04-30 15:14:51 +0700

    colliding variable name changed

--- main/leetcode/median_of_two_sorted_arrays.go
+++ main/leetcode/median_of_two_sorted_arrays.go
@@ -8,9 +8,9 @@ func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
 	if len(nums) == 1 {
 		return float64(nums[0])
 	}
-	len := len(nums)
-	halfLen := len / 2
-	if len%2 == 0 {
+	length := len(nums)
+	halfLen := length / 2
+	if length%2 == 0 {
 		return float64(nums[halfLen-1]+nums[halfLen]) / 2
 	} else {
 		return float64(nums[halfLen])
